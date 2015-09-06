b8d9cd14bc40c2255e07753cbb95695973979e0a
Leo Tao <taoxb@163.com>
2015-09-06 12:39:53 +0800

    a new c project

---
+++ TestCommandC/TestCommandC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TestCommandC
+//
+//  Created by Leo Tao on 9/6/15.
+//  Copyright (c) 2015 Leo Tao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World, This is first C App on Mac!\n");
+    return 0;
+}
